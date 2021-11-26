4a226939f567ea3860367e80a1e4fb54a61d640a
root <root@DESKTOP-HOME.localdomain>
2021-11-26 17:01:55 +0000

    Shortened code

--- paml.py
+++ paml.py
@@ -3,16 +3,15 @@ from collections import ChainMap
 from dataclasses import dataclass
 from pprint import pprint
 from argparse import ArgumentParser
-import itertools as it
-import operator as op
-import math
-import json
+import itertools as it, operator as op, math, json, platform
+
+__version__ = '0.2.4'
 
 parser = Lark(r"""
 _seperated{elem, sep}: [ elem (sep elem)* ]
 start: (extern_expr | assignment)*
 
-?extern_expr: "::" expr
+extern_expr: "::" expr
 ?expr: literal
     | method
     | expr "(" _seperated{expr, ","} ")"                    -> func_call
@@ -36,18 +35,13 @@ COMMENT: /#.*/
 %import common.WS
 %ignore WS
 %ignore COMMENT
-""")
-
-def cleanup_ret(d):
-    return dict(zip(map(str, d.keys()), d.values()))
+""", propagate_positions=True)
 
 class AttrDict(dict):
-    def __init__(self, d):
-        super().__init__(cleanup_ret(d))
+    __init__ = lambda self, d: super().__init__(dict(zip(map(str, d.keys()), d.values())))
+    __getattr__ = lambda self, k: self[k]
 
-    def __getattr__(self, k):
-        return self[k]
-
+FILE_CONTENT = ''
 ENV = ChainMap({}, {
     'true': True,
     'false': False,
@@ -62,44 +56,22 @@ ENV = ChainMap({}, {
     })
 })
 
-class Function:
-    def __init__(self, args, tree):
-        self.args = args
-        self.tree = tree
-
-    def __call__(self, env, *a):
-        res = complete_transform(self.tree, env.new_child(dict(zip(self.args, a))))
-        return res
-
-class CompiledIfExpr:
-    def __init__(self, cond, e1, e2):
-        self.cond = cond
-        self.e1 = e1
-        self.e2 = e2
-
-    def __call__(self, env):
-        cond = complete_transform(self.cond, env)
-        return complete_transform(self.e1 if cond else self.e2, env)
-
-class CompiledMap:
-    def __init__(self, tree):
-        self.tree = tree
-
-    def __call__(self, env):
-        return AttrDict(complete_transform(self.tree, env.new_child()))
-
 class FunctionTransformer(Transformer):
     def make_func(self, v):
-        *a, e = v
-        return Function(a, e)
+        *args, tree = v
+        return (lambda env, *a: complete_transform(tree, env.new_child(dict(zip(args, a)))))
 
     def if_expr(self, v):
         _cond, e1, e2 = v
-        return Tree('compiled_if_expr', [CompiledIfExpr(_cond, e1, e2)])
+        return Tree('compiled_if_expr', [lambda env: complete_transform(e1 if complete_transform(_cond, env) else e2, env)])
+
+    def extern_expr(self, v):
+        tree, = v
+        return Tree('compiled_extern_expr', [lambda env: [print(":: " + FILE_CONTENT[tree.meta.start_pos:tree.meta.end_pos]), complete_transform(tree, env)][1]])
 
     def map(self, v):
         tree, = v
-        return Tree('compiled_map', [CompiledMap(tree)])
+        return Tree('compiled_map', [lambda env: AttrDict(complete_transform(tree, env.new_child()))])
 
 class PAMLTransformer(Transformer):
     def __init__(self, env):
@@ -110,16 +82,16 @@ class PAMLTransformer(Transformer):
         return self.env.maps[0]
 
     def compiled_if_expr(self, v):
-        if_expr, = v
-        return if_expr(self.env)
+        return v[0](self.env)
 
     def compiled_map(self, v):
-        let_expr, = v
-        return let_expr(self.env)
+        return v[0](self.env)
+
+    def compiled_extern_expr(self, v):
+        return v[0](self.env)
 
     def assignment(self, v):
-        name, e = v
-        self.env[name] = e
+        self.env[v[0]] = v[1]
 
     def literal(self, v):
         return eval(v[0])
@@ -130,25 +102,25 @@ class PAMLTransformer(Transformer):
 
     def func_call(self, v):
         e, *a = v
-        if type(e) == Function:
+        if 'env' in e.__code__.co_varnames:     # Hacky, but who cares when brevity is on the line?
             return e(self.env, *a)
         else:
             return e(*a)
 
     def get_var(self, v):
-        name, = v
-        return self.env[name]
-
-    def map(self, v):
-        return AttrDict(zip(v[::2], vp[1::2]))
-
+        return self.env[v[0]]
 
 def complete_transform(t, env):
     return PAMLTransformer(env).transform(FunctionTransformer().transform(t))
 
+def run_str(s, env):
+    return complete_transform(parser.parse(s), env)
+
 def import_module(filename):
+    global FILE_CONTENT
     with open(filename) as f:
-        return AttrDict(cleanup_ret(complete_transform(parser.parse(f.read()), ENV)))
+        FILE_CONTENT = f.read()
+    return AttrDict(run_str(FILE_CONTENT, ENV))
 
 def is_json_serializable(o):
     try:
@@ -180,6 +152,28 @@ def run_file(f):
     print('\n======= DATA =======')
     pprint(d)
 
+def repl(env=ENV):   # TODO: UNSTABLE, FIX
+    print(f'Welcome to PAML {__version__}!\nType :h for help or :q to exit.')
+    while True:
+        s = input('> ')
+        if s[0] == ':':
+            if s[1] == 'q': break
+            elif s[1] == 'h': print('Commands: :q (exit), :h (help), :e (print environment)')
+            elif s[1] == 'e': pprint(env)
+        else:
+            while not s.endswith(';;'):
+                s += input('  ')
+            s = s[:-2]
+            try:
+                res = run_str(s, env)
+            except:
+                res = run_str(':: ' + s, env)
+            env['_'] = res
+            pprint(res)
+        s = ''
+
+# Testing code
+
 n = 5
 FILE = f'test/test{n}.paml'
 
@@ -188,7 +182,8 @@ def tester():
     print(sample_code)
     parsed = parser.parse(sample_code)
     print(parsed.pretty())
-    res = PAMLTransformer(ENV).transform(FunctionTransformer().transform(parsed))
+    # res = PAMLTransformer(ENV).transform(FunctionTransformer().transform(parsed))
+    res = AttrDict(complete_transform(parsed, ENV))
     print(res)
 
 def main():
@@ -197,6 +192,6 @@ def main():
     argparser.add_argument('--to-json')
 
 if __name__ == '__main__':
-    with open(FILE) as f:
-        print(f.read())
-    to_json(import_module(FILE), f'outputs/test{n}.json')
+    # tester()
+    # run_file(FILE)
+    repl()
